ca13f2d94209fa1e6ee39e8a74d68a39ac1e9ab1
Emmanuel Kofi Agyapong <agiapong@gmail.com>
2020-12-28 18:17:38 -0500

    General updates on layouts and in MainActivity

--- app/src/main/java/com/thewizard91/thejournal/MainActivity.java
+++ app/src/main/java/com/thewizard91/thejournal/MainActivity.java
@@ -2,13 +2,41 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+//Firebase imports
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Firebase instances
+    FirebaseFirestore cloudBaseDatabase;
+    FirebaseAuth userAuthorized;
+    FirebaseUser currentUser;
+
+    //Normal object instances
+    private String currentUserId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Checking if the user is already logged in
+        userAuthorized = FirebaseAuth.getInstance();
+        cloudBaseDatabase = FirebaseFirestore.getInstance();
+        currentUser = userAuthorized.getCurrentUser();
+        if(currentUser != null) {
+            // TODO: Move on with the next stuff
+        }
+        sendToLoginActivity();
+    }
+
+    private void sendToLoginActivity() {
+        // Start the Login Activity
+        startActivity(new Intent(this, LogInActivity.class));
     }
 }
