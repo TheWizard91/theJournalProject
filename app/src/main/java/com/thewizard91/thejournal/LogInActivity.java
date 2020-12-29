76a528cc67e58ce942336921f7dcf1ef97f677fc
Emmanuel Kofi Agyapong <agiapong@gmail.com>
2020-12-29 18:07:49 -0500

    ForgotPasswordActivity added updated the LogInActivity

--- app/src/main/java/com/thewizard91/thejournal/LogInActivity.java
+++ app/src/main/java/com/thewizard91/thejournal/LogInActivity.java
@@ -1,14 +1,121 @@
 package com.thewizard91.thejournal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.thewizard91.thejournal.log_in_activities_adds_on.ForgotPasswordActivity;
+
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Objects;
 
 public class LogInActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+    private EditText enterEmailOrUserName;
+    private EditText password;
+    private ImageButton logInButton;
+    private TextView forgotPassword;
+    private TextView needAnAccount;
+    private TextView useSocialMediaAccountToLogIn;
+
+    private FirebaseAuth userAuthorized;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        progressBar = findViewById(R.id.log_in_progress_bar_id);
+        enterEmailOrUserName = findViewById(R.id.log_in_enter_email_or_username_text_view_id);
+        password = findViewById(R.id.log_in_enter_your_password_text_view_id);
+        logInButton = findViewById(R.id.log_in_image_button_id);
+        forgotPassword = findViewById(R.id.log_in_forgot_password_id);
+        needAnAccount = findViewById(R.id.log_in_new_account_id);
+        useSocialMediaAccountToLogIn = findViewById(R.id.log_in_with_social_media_account_id);
+
+        userAuthorized = FirebaseAuth.getInstance();
+        triggerLogInButton();
+        triggerForgotPasswordText();
+        triggerNeedAccountText();
+        triggerLogInWithSocialMediaAccount();
+    }
+
+    private void triggerLogInWithSocialMediaAccount() {
+        // Set the user to the activity that will allow the user to log in w social media accounts.
+    }
+
+    private void triggerNeedAccountText() {
+        needAnAccount.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendToSigUpActivity();
+            }
+        });
+    }
+
+    private void sendToSigUpActivity() {
+        startActivity(new Intent(this, SignUpActivity.class));
+        finish();
+    }
+
+    private void triggerForgotPasswordText() {
+        // Set the user to activity that will update the password.
+        forgotPassword.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendToForgotPasswordActivity();
+            }
+        });
+    }
+
+    private void sendToForgotPasswordActivity() {
+        startActivity(new Intent(this, ForgotPasswordActivity.class));
+        finish();
+    }
+
+    private void triggerLogInButton() {
+        logInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailOrUsernameInserted = enterEmailOrUserName.getText().toString();
+                String passwordInserted = password.getText().toString();
+                if (!emailOrUsernameInserted.isEmpty() && !passwordInserted.isEmpty()){
+                    userAuthorized.signInWithEmailAndPassword(emailOrUsernameInserted, passwordInserted)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @SuppressLint("WrongConstant")
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful()){
+                                        progressBar.setVisibility(0);
+                                        sendToMainActivity();
+                                    }
+                                    Toast.makeText(LogInActivity.this, "Error: "+((Exception)Objects.requireNonNull(task.getException()))
+                                            .getMessage(), 0).show();
+                                }
+                            });
+                }
+            }
+        });
+    }
+
+    private void sendToMainActivity() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
     }
 }
