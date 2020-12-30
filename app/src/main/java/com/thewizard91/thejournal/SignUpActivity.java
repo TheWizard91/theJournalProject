52dc6af6603992aaa7f55394a7503e4b48e75a8d
Emmanuel Kofi Agyapong <agiapong@gmail.com>
2020-12-30 01:07:46 -0500

    Added AccountSettingsActivity

--- app/src/main/java/com/thewizard91/thejournal/SignUpActivity.java
+++ app/src/main/java/com/thewizard91/thejournal/SignUpActivity.java
@@ -1,14 +1,113 @@
 package com.thewizard91.thejournal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.thewizard91.thejournal.sing_up_adds_on.AccountSettingsActivity;
+
+import android.content.Intent;
+
+import java.util.Objects;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    private ProgressBar progressBar;
+    private EditText enterEmail;
+    private EditText enterPassword;
+    private EditText confirmPassword;
+    private TextView haveAnAccountAlready;
+    private ImageButton signUpButton;
+    
+    private FirebaseAuth userAuthorized;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        
+        progressBar = findViewById(R.id.sign_up_progress_bar_id);
+        enterEmail = findViewById(R.id.sign_up_mail_address_text_view_id);
+        enterPassword = findViewById(R.id.sign_up_password_text_view_id);
+        confirmPassword = findViewById(R.id.sign_password_confirmation_id);
+        haveAnAccountAlready = findViewById(R.id.already_have_an_account_message_id);
+        signUpButton = findViewById(R.id.sign_up_button_id);
+        userAuthorized = FirebaseAuth.getInstance();
+        finishTheLogInActivity();
+        createANewAccount();
+        triggerHaveAlreadyAnAccount();
+    }
+
+    private void triggerHaveAlreadyAnAccount() {
+        haveAnAccountAlready.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendToLogInActivity();
+            }
+        });
+    }
+
+    private void sendToLogInActivity() {
+        startActivity(new Intent(this, LogInActivity.class));
+    }
+
+    private void createANewAccount() {
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("WrongConstant")
+            @Override
+            public void onClick(View v) {
+                String emailInserted = enterEmail.getText().toString();
+                String passwordInserted = enterPassword.getText().toString();
+                String passwordConfirmed = confirmPassword.getText().toString();
+                if(!TextUtils.isEmpty(emailInserted) && !TextUtils.isEmpty(passwordConfirmed) && !TextUtils.isEmpty(passwordConfirmed)){
+                    if(passwordInserted.equals(passwordConfirmed)){
+                        progressBar.setVisibility(0);
+                        userAuthorized.createUserWithEmailAndPassword(emailInserted, passwordInserted)
+                                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                    @SuppressLint("WrongConstant")
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if(task.isSuccessful()){
+                                            sendToAccountSettingsActivity();
+                                        } else {
+                                            Toast.makeText(SignUpActivity.this,
+                                                    "Error: "+((Exception) Objects.requireNonNull(task.getException()))
+                                            .getMessage(), 5).show();
+                                        }
+                                        progressBar.setVisibility(4);
+                                    }
+                                });
+                    }
+                    Toast.makeText(SignUpActivity.this, "Passwords Do Not Match", 0).show();
+                }
+            }
+        });
+    }
+
+    private void sendToAccountSettingsActivity() {
+        startActivity(new Intent(this, AccountSettingsActivity.class));
+    }
+
+    private void finishTheLogInActivity() {
+        haveAnAccountAlready.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/thewizard91/thejournal/sing_up_adds_on/AccountSettingsActivity.java
@@ -0,0 +1,16 @@
+package com.thewizard91.thejournal.sing_up_adds_on;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.thewizard91.thejournal.R;
+
+public class AccountSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_settings);
+    }
+}
